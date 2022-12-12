3788fb2a790dac5f81b9810e7b8359992f2c5cd2
neprostitelno <shuakiko9997@gmail.com>
2022-12-12 11:19:17 +0600

    frontend fix

--- frontend/src/Components/AllAuthors/AllAuthors.jsx
+++ frontend/src/Components/AllAuthors/AllAuthors.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import s from "./AllAuthors.module.css"
 
 const AllAuthors = (props) => {
 
     return <div>
-        <div>
+        <div className={s.item}>
             {props.authors.map(a => <div key={a.id}>
                 <NavLink to={'/author/' + a.id}>
                     {a.surname} {a.name} {a.patronymic}
