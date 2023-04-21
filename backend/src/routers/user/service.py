cc798abdbd4e7f18b733f6e587e7243ff4d87389
Valery <korolevvalery37@gmail.com>
2023-04-21 10:35:16 +0600

    cookie auth

--- backend/src/routers/user/service.py
+++ backend/src/routers/user/service.py
@@ -1,4 +1,9 @@
-from fastapi import security
+from typing import Optional
+
+from fastapi import security, Request, status, HTTPException
+from fastapi.security import OAuth2
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.security.utils import get_authorization_scheme_param
 from sqlalchemy.orm import Session
 import jwt as _jwt
 
@@ -24,4 +29,52 @@ async def service_get_user_by_id(id: int, db: Session):
 
 
 def service_oauth2scheme():
-    return security.OAuth2PasswordBearer(tokenUrl="/api/user/token")
+    return OAuth2PasswordBearerCookie(tokenUrl="/api/user/token")
+
+
+class OAuth2PasswordBearerCookie(OAuth2):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: str = None,
+        scopes: dict = None,
+        auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
+        super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        header_authorization: str = request.headers.get("Authorization")
+        cookie_authorization: str = request.cookies.get("Authorization")
+
+        header_scheme, header_param = get_authorization_scheme_param(
+            header_authorization
+        )
+        cookie_scheme, cookie_param = get_authorization_scheme_param(
+            cookie_authorization
+        )
+
+        if header_scheme.lower() == "bearer":
+            authorization = True
+            scheme = header_scheme
+            param = header_param
+
+        elif cookie_scheme.lower() == "bearer":
+            authorization = True
+            scheme = cookie_scheme
+            param = cookie_param
+
+        else:
+            authorization = False
+
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated"
+                )
+            else:
+                return None
+        return param
+
